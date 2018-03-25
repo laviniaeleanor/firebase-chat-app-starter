745daedcf7ffdbae53447c146256bd0e5374cc54
laviniaeleanor <laviniaeleanor@outlook.com>
2018-03-25 01:42:45 +0100

    Change css, simplify js and add event listener

--- scripts/chat.js
+++ scripts/chat.js
@@ -1,13 +1,20 @@
 
-function replaceForm(){
-    var form = document.getElementById("welcome");
-    console.log(form);
-    form.innerHTML = `<p>Bienvenus!!</p> </br> <a href= 'index.html'><p> Enter le chat room!</p></a>`;
-};
 
 function submitUsername(){
     var username = document.getElementById("username").value;
     console.log(username);
-    ChatApp.createOrUpdateUser(document.getElementById("username").value, -1, function(userData) { console.log(userData)} );
-    replaceForm();
+    ChatApp.createOrUpdateUser(username, -1, function(userData) { console.log(userData)} );
+}
+
+function submitMessage(){
+    var username = document.getElementById("username").value;
+    var message = document.getElementById("messageBox").value;
+    ChatApp.newMessage(username, message);
+    console.log(username, message);
+}
+
+ChatApp.addMessageListener(handleMessage);
+
+function handleMessage (changeType, messageId, messageData) {
+    document.getElementById("dbMessages").innerText += `${messageData.username} : ${messageData.text}\n`;
 }
