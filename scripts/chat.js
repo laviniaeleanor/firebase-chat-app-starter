765ba88ef6828060614098ffb421ab2084f03842
laviniaeleanor <laviniaeleanor@outlook.com>
2018-03-26 09:24:40 +0200

    Timestamp working

--- scripts/chat.js
+++ scripts/chat.js
@@ -17,14 +17,14 @@ ChatApp.addMessageListener(handleMessage);
 
 function handleMessage (changeType, messageId, messageData) {
     var username = document.getElementById("username").value;
-    //var timestamp = `${messageData.timestamp.getHours()} : ${messageData.timestamp.getMinutes()}, <strong>${messageData.timestamp.getDate()} ${messageData.timestamp.getMonth()}</strong> `;
+    var timestamp = `${messageData.timestamp.getHours()}:${messageData.timestamp.getMinutes()}, ${messageData.timestamp.getDate()}.${messageData.timestamp.getMonth()}.${messageData.timestamp.getYear()}. `;
     function callbackUserDatabase(userData) {
-        return userData;
+        return userData.color;
         console.log(userData);
     };
         var userColor = ChatApp.getUser(username, callbackUserDatabase(username));
         console.log(userColor);
-    document.getElementById("dbMessages").innerHTML += `<p class="messageSpace" style = "color: ${userColor}">${messageData.timestamp}<br><strong>${messageData.userName}</strong> : ${messageData.text}\n</p>`;
+    document.getElementById("dbMessages").innerHTML += `<p class="messageSpace" style = "color: ${userColor}">${timestamp}<br><strong>${messageData.userName}</strong> : ${messageData.text}</p><br>`;
 
 }
 
@@ -39,8 +39,6 @@ function handleActiveUsers (changeType,messageId, messageData) {
 
     if (timeNow - timeMessage <= (5*60*1000)) {
         var activeUsersSpace = document.getElementById("activeUsers");
-        if (activeUsersSpace.innerHTML.indexOf(user) !== -1) {
         activeUsersSpace.innerHTML += `<p><strong>${user}</strong>\n</p>`;
-        }
     }
 }
